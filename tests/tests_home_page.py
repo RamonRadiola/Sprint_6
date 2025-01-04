0b4026f0ebd63b48ede2a0160fb21ec696af3ffc
RamonRadiola <ramon0990@yandex.ru>
2025-01-04 13:10:20 +0300

    сократил тест за счет использования параметризованного метода

--- tests/tests_home_page.py
+++ tests/tests_home_page.py
@@ -1,47 +1,14 @@
 import allure
+import pytest
+from pages.home_page import HomePage
+from data import Data
 
-from locators import Locators
-
-@allure.story("Тестируем выпадающий список раздела с вопросами и ответами")
+@allure.story("Тестирует выпадающий список раздела с вопросами и ответами")
 class TestHomePage:
-    @allure.step("Открываем первый вопрос")
-    def test_question_0(self, home_page, driver):
-        home_page.click_place_0()
-        home_page.check_answer_0()
-        assert 'Сутки — 400 рублей.' in driver.find_element(*Locators.ANSWER_0).text
-
-    @allure.step("Открываем второй вопрос")
-    def test_question_1(self, home_page, driver):
-        home_page.click_place_1()
-        home_page.check_answer_1()
-        assert 'Пока что у нас так' in driver.find_element(*Locators.ANSWER_1).text
-
-    @allure.step("Открываем третий вопрос")
-    def test_question_2(self, home_page, driver):
-        home_page.click_place_2()
-        home_page.check_answer_2()
-        assert 'вы оформляете заказ' in driver.find_element(*Locators.ANSWER_2).text
-
-    @allure.step("Открываем четвертый вопрос")
-    def test_question_3(self, home_page, driver):
-        home_page.click_place_3()
-        home_page.check_answer_3()
-        assert 'начиная с завтрашнего дня' in driver.find_element(*Locators.ANSWER_3).text
-
-    @allure.step("Открываем пятый вопрос")
-    def test_question_4(self, home_page, driver):
-        home_page.click_place_4()
-        home_page.check_answer_4()
-        assert 'Пока что нет' in driver.find_element(*Locators.ANSWER_4).text
-
-    @allure.step("Открываем шестой вопрос")
-    def test_question_5(self, home_page, driver):
-        home_page.click_place_5()
-        home_page.check_answer_5()
-        assert 'Самокат приезжает к вам' in driver.find_element(*Locators.ANSWER_5).text
-
-    @allure.step("Открываем седьмой вопрос")
-    def test_question_6(self, home_page, driver):
-        home_page.click_place_6()
-        home_page.check_answer_6()
-        assert 'пока самокат не привезли' in driver.find_element(*Locators.ANSWER_6).text
+    @allure.story("Кликает на плашку вопроса, проверяет текст выпадающего ответа")
+    @pytest.mark.parametrize("question_data", Data.QUESTION_DATA)
+    def test_question_and_answer(self, home_page_start, question_data):
+        home_page = HomePage(home_page_start.driver)
+        home_page.click_question(question_data["question_locator"])
+        answer_text = home_page.get_answer_text(question_data["answer_locator"])
+        assert question_data["expected_text"] in answer_text
